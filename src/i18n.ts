9b6a0ff9d9f92601f72b462e1f0fb1562fda0cd1
Janos Molnar <standby929@gmail.com>
2025-07-25 18:34:01 +0200

    refactor: use command parameter for language support

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,19 @@ import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 import path from 'path';
 
-export async function initI18n() {
+export async function initI18n(lng: string = 'hu') {
+  if (i18next.isInitialized && i18next.language === lng) return;
+
   await i18next.use(Backend).init({
-    lng: 'en',
-    fallbackLng: 'en',
+    lng,
+    fallbackLng: 'hu',
     backend: {
       loadPath: path.join(__dirname, '../locales/{{lng}}.json'),
     },
     interpolation: {
       escapeValue: false,
     },
+    initImmediate: false, // priority matters
   });
 }
 
